90dbf36c25c93518833493581c5a9af74a7cec7b
murrayn808 <murrayn@hawaii.edu>
2022-06-29 03:12:54 -1000

    made a slider

--- django-application/mysite/settings.py
+++ django-application/mysite/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -106,8 +107,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
